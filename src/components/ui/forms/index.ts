77883a756d8d05d8fe4133bb182b99ada0c48bd2
Vlada <vladam3636@gmail.com>
2025-04-29 12:41:02 +0200

    deleted unneccessary exports

--- src/components/ui/forms/index.ts
+++ src/components/ui/forms/index.ts
@@ -1,5 +1,2 @@
-export * from "./AddMemberForm.tsx";
-export * from "./CreateDeviceForm.tsx";
-export * from "./CreateHouseholdForm.tsx";
 export * from "./LoginForm.tsx";
 export * from "./RegisterForm.tsx";

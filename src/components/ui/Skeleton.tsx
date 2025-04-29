886eb0d98625edc34ad77ebc00748ed1e2b85818
Vlada <vladam3636@gmail.com>
2025-04-29 12:33:23 +0200

    shadcn set up and istall of components

--- src/components/ui/Skeleton.tsx
+++ src/components/ui/Skeleton.tsx
@@ -1,10 +1,13 @@
-import * as React from "react";
+import { cn } from "@/lib/utils"
 
-interface SkeletonProps {
-  w: number;
-  h: number;
+function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="skeleton"
+      className={cn("bg-accent animate-pulse rounded-md", className)}
+      {...props}
+    />
+  )
 }
 
-export const Skeleton: React.FC<SkeletonProps> = ({ w, h }) => {
-  return <div style={{ width: w, height: h }}></div>;
-};
+export { Skeleton }
